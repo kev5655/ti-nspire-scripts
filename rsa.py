a835bb0d6ef8be015e910a2cb862d93d604a7298
Kevin-Zahn <48159968+kev5655@users.noreply.github.com>
2023-12-28 18:19:06 +0100

    fix program for ti

--- rsa.py
+++ rsa.py
@@ -4,56 +4,6 @@ from math import *
 # ToDo is gcd()
 
 
-def main():
-    p = val_input("Enter prime p=")
-    q = val_input("Enter prime q=")
-
-    n = val_input("Enter prime factor n=")
-
-    e = val_input("Enter pub key e=")
-    d = val_input("Enter priv key d=")
-
-    m = val_input("Enter message m=")
-    c = val_input("Enter encrypted msg c=")
-
-    if p is None and q is None:
-        (p, q) = factorize(n)
-        print("factorized p=" + p + " q=" + p)
-    elif p is None:
-        p = n / q
-        print("p=" + p)
-    elif q is None:
-        q = n / p
-        print("q=" + q)
-
-    if n is None:
-        n = p * q
-
-    if e is None:
-        if d is None:
-            print("Error d is None d=" + d)
-            exit
-        e = wrap_extended_euclaid(d, calc_phi(p, q))
-    if d is None:
-        if e is None:
-            print("Error e is None e=" + e)
-            exit
-        d = wrap_extended_euclaid(e, calc_phi(p, q))
-    print("calc e=" + e + ", d=" + d)
-
-    if c is None and m is None:
-        print("Error c and m is None")
-        exit
-
-    if c is None:
-        c = pow(m, e, n)
-    if m is None:
-        m = pow(c, d, n)
-
-    print("m=" + m + " c=" + c + " e=" + e + " d=" +
-          d + " n=" + n + " p=" + p + " q= " + q)
-
-
 def factorize(n: int):
     k = int(ceil(sqrt(n)))
     for i in range(1000):
@@ -66,21 +16,21 @@ def factorize(n: int):
     print("Error no factor found for n=" + n)
 
 
+def extended_euclaid(a: int, b: int) -> tuple:
+    assert (gcd(a, b) == 1.0)
+    if a == 0:
+        return b, 0, 1
+    else:
+        gcd_val, x, y = extended_euclaid(b % a, a)
+        return gcd_val, y - (b // a) * x, x
+
+
 def wrap_extended_euclaid(a: int, n: int) -> int:
     res = extended_euclaid(a, n)
     b = res[1]
     while b < 0:
         b += n
     return b
-
-
-def extended_euclaid(a: int, b: int) -> tuple:
-    assert (gcd(a, b) == 1.0)
-    if a == 0:
-        return b, 0, 1
-    else:
-        gcd, x, y = extended_euclaid(b % a, a)
-        return gcd, y - (b // a) * x, x
 
 
 def calc_phi(p: int, q: int) -> int:
@@ -95,8 +45,53 @@ def val_input(prompt: str):
         number = int(inp)
         return number
     except ValueError:
-        print("Error -> " + prompt + "is a string")
+        print("Error -> " + str(prompt) + "is a string")
 
 
-if __name__ == "__main__":
-    main()
+p = val_input("Enter prime p=")
+q = val_input("Enter prime q=")
+
+n = val_input("Enter prime factor n=")
+
+e = val_input("Enter pub key e=")
+d = val_input("Enter priv key d=")
+
+m = val_input("Enter message m=")
+c = val_input("Enter encrypted msg c=")
+
+if p is None and q is None:
+    (p, q) = factorize(n)
+    print("factorized p=" + str(p) + " q=" + str(p))
+elif p is None:
+    p = n / q
+    print("p=" + str(p))
+elif q is None:
+    q = n / p
+    print("q=" + str(q))
+
+if n is None:
+    n = p * q
+
+if e is None:
+    if d is None:
+        print("Error d is None d=" + str(d))
+        exit
+    e = wrap_extended_euclaid(d, calc_phi(p, q))
+if d is None:
+    if e is None:
+        print("Error e is None e=" + str(e))
+        exit
+    d = wrap_extended_euclaid(e, calc_phi(p, q))
+print("calc e=" + str(e) + ", d=" + str(d))
+
+if c is None and m is None:
+    print("Error c and m is None")
+    exit
+
+if c is None:
+    c = pow(m, e, n)
+if m is None:
+    m = pow(c, d, n)
+
+print("m=" + str(m) + " c=" + str(c) + " e=" + str(e) + " d=" +
+      str(d) + " n=" + str(n) + " p=" + str(p) + " q= " + str(q))
