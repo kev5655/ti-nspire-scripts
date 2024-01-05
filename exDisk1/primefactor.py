7923cee4a48514d6281175e20b4c3e7824522947
Kevin-Zahn <48159968+kev5655@users.noreply.github.com>
2024-01-05 15:58:41 +0100

    update by rsa

--- exDisk1/primefactor.py
+++ exDisk1/primefactor.py
@@ -1,4 +1,4 @@
-import math
+from math import sqrt, ceil, floor
 
 n = float(input("Enter n="))
 k = float(input("Enter start k="))
@@ -7,14 +7,21 @@ x = 0
 it = 0
 
 
-for i in range(1000):
-    it = i
-    t = pow(k + i, 2) - n
-    sqr = math.sqrt(t)
-    isInt = sqr == math.floor(sqr)
-    if isInt:
-        x = i
-        break
-
-print(f"iterationen {it}")
-print(f"({k} + {x})^2 - {n} = {t} = {sqr}^2")
+def factorize(n: int):
+    k = int(ceil(sqrt(n)))
+    print(k)
+    for i in range(1000):
+        print(i)
+        t = pow(k + i, 2) - n
+        sqr = sqrt(t)
+        if sqr == floor(sqr):  # check is ganze Zahl
+            p = (k + i) + int(sqr)
+            q = (k + i) - int(sqr)
+            print("factorized p=" + str(p) + " q=" + str(q))
+            print("--("+str(k)+" + " +
+                  str(i) + ")^2 - " + str(n) + " = " + str(t) + " => sqrt(" + str(t) + ") = " + str(floor(sqr)))
+            print("--" + str(k + i) + "^2 - " + str(t) + "^2 = " + str(n) + "")
+            print("--(" + str(k + i) + " + " + str(floor(sqr)) +
+                  ") * (" + str(k + i) + " - " + str(floor(sqr)) + ") = " + str(n))
+            return (p, q)
+    print("Error no factor found for n=" + str(n))
