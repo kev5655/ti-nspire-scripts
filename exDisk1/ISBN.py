db6f584e3eb2242100ce0be48c08a1816839f6e6
chrugi-main <118556340+chrugi-main@users.noreply.github.com>
2024-01-05 20:17:01 +0100

    Add files via upload
    Script to test if ISBN is correct and prints sum mod 11 of the number.

---
+++ exDisk1/ISBN.py
@@ -0,0 +1,19 @@
+def isISBNRight(*numbers):
+    if len(numbers) != 10:
+        return False
+    else:
+        total = 0
+        for i in range(1, len(numbers) + 1):
+            total += i * int(numbers[i - 1])
+        print("Sum mod 11 =", total, "mod 11 =", total % 11)
+        return total % 11 == 0
+
+print("Please enter the ISBN z1,z2,z3,z4,z5,z6,z7,z8,z9,z10:")
+isbn_input = input()
+isbn_numbers = [int(x) for x in isbn_input.split(',')]
+result = isISBNRight(*isbn_numbers)
+
+if result:
+    print("The ISBN is correct.")
+else:
+    print("The ISBN is not correct.")
