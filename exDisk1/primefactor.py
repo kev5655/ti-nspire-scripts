94369917789cc3171fbf40c2c85d16b517a39ec1
Kevin-Zahn <48159968+kev5655@users.noreply.github.com>
2024-01-06 14:05:24 +0100

    wrap with function

--- exDisk1/primefactor.py
+++ exDisk1/primefactor.py
@@ -1,22 +1,26 @@
 from math import sqrt, ceil, floor
 
-n = float(input("Enter n="))
+n = int(input("Enter n="))
 
 
-k = int(ceil(sqrt(n)))
-for i in range(1000):
-    print(i)
-    t = pow(k + i, 2) - n
-    sqr = sqrt(t)
-    if sqr == floor(sqr):  # check is ganze Zahl
-        p = (k + i) + int(sqr)
-        q = (k + i) - int(sqr)
-        print("factorized p=" + str(p) + " q=" + str(q))
-        print("--("+str(k)+" + " +
-              str(i) + ")^2 - " + str(n) + " = " + str(t) + " => sqrt(" + str(t) + ") = " + str(floor(sqr)))
-        print("--" + str(k + i) + "^2 - " + str(t) + "^2 = " + str(n) + "")
-        print("--(" + str(k + i) + " + " + str(floor(sqr)) +
-              ") * (" + str(k + i) + " - " + str(floor(sqr)) + ") = " + str(n))
-        break
+def primefactor(n: int) -> tuple:
+    k = int(ceil(sqrt(n)))
+    for i in range(1000):
+        print(i)
+        t = pow(k + i, 2) - n
+        sqr = sqrt(t)
+        if sqr == floor(sqr):  # check is ganze Zahl
+            p = (k + i) + int(sqr)
+            q = (k + i) - int(sqr)
+            print("factorized p=" + str(p) + " q=" + str(q))
+            print("--("+str(k)+" + " +
+                  str(i) + ")^2 - " + str(n) + " = " + str(t) + " => sqrt(" + str(t) + ") = " + str(floor(sqr)))
+            print("--" + str(k + i) + "^2 - " + str(t) + "^2 = " + str(n) + "")
+            print("--(" + str(k + i) + " + " + str(floor(sqr)) +
+                  ") * (" + str(k + i) + " - " + str(floor(sqr)) + ") = " + str(n))
+            return (p, q)
+
+
+(p, q) = primefactor(n)
 if p == None or q == None:
     print("Error no factor found for n=" + str(n))
