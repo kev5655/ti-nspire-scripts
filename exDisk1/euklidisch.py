bbe44e4f0821829756d8497593e5390b6c1eaa26
Kevin-Zahn <48159968+kev5655@users.noreply.github.com>
2024-01-07 01:04:57 +0100

    not finish extended_euclaid

--- exDisk1/euklidisch.py
+++ exDisk1/euklidisch.py
@@ -1,7 +1,16 @@
 from math import floor
 
-a = int(input("a="))
-b = int(input("b="))
+a = 50  # int(input("a="))
+b = 80  # int(input("b="))
+
+val_mapper = {
+    str(a): "a",
+    str(b): "b"
+}
+# val_mapper = [10000]
+# val_mapper[a] = "a"
+# val_mapper[b] = "b"
+
 
 store = []
 
@@ -32,11 +41,87 @@ def euclaid(a: int, b: int):
     return store
 
 
-def extended_euclaid():
+def extended_euclaid(store):
+    for x in store:
+        print(x["rest"], "=", x["result"], "-", x["base"])
+        res = val_mapper[str(x["result"])]
+        base = val_mapper[str(x["base"])]
+        val_mapper[str(x["rest"])] = "(" + str(res) + "-" + str(base) + ")"
+        alge = str(res) + "-" + str(base)
+        # if (alge.__contains__("(")):
+        #     (a, b) = alge.split("(")
+        #     if (a.__contains__("-")):
+        #         alge = solveBrackets("-", "(" + b)
+        #     else:
+        #         alge = solveBrackets("+", "(" + b)
+        #     alge = [*a][0] + alge
+        # if (alge.count("a") > 1 or alge.count("b") > 1):
+        # alge = simplify_expression(alge)
+
+        print(str(x["rest"]) + "=" + alge)
+
     exit
 
 
-x = euclaid(a, b)
+def solveBrackets(sign: str, alge: str):
+    alge = alge.replace("(", "").replace(")", "")
+    print(alge)
+    if (alge.__contains__("-")):
+        (a, b) = alge.split("-")
+        b = "-" + b
+    else:
+        (a, b) = alge.split("+")
+        b = "+" + b
 
-for i in x:
-    print(i)
+    if (sign == "-"):
+        a = sign + a
+        if ([*b][0] == "-"):
+            b = "+" + [*b][1]
+        else:
+            b = "-" + [*b][1]
+        return a + b
+
+
+# def simplify_expression(expr):
+#     terms = expr.replace('-', '+-').split('+')
+#     term_dict = {}
+
+#     for term in terms:
+#         if term:
+#             # Identify the coefficient and variable
+#             if term[-1].isalpha():
+#                 var = term[-1]
+#                 coef = term[:-1]
+#                 coef = int(coef) if coef != '-' else -1
+#             else:
+#                 var = ''
+#                 coef = int(term)
+
+#             # Combine like terms
+#             if var in term_dict:
+#                 term_dict[var] += coef
+#             else:
+#                 term_dict[var] = coef
+
+#     # Construct simplified expression
+#     simplified_expr = ''
+#     for var, coef in term_dict.items():
+#         if coef == 0:
+#             continue
+#         elif coef == 1 and var:
+#             simplified_expr += '+' + var
+#         elif coef == -1 and var:
+#             simplified_expr += '-' + var
+#         else:
+#             simplified_expr += ('+' if coef > 0 else '') + str(coef) + var
+
+#     return simplified_expr.lstrip('+')
+
+# print(solveBrackets("-", "(a-b)"))
+
+
+store = euclaid(a, b)
+for i in range(len(store)):
+    print(store[i])
+print(val_mapper)
+extended_euclaid(store)
